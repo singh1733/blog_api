d6fe2498a3e0c232b3c8ba2242558d4619f7fa9b
singh1773 <singh.1733@osu.edu>
2025-01-01 11:45:48 -0500

    started post route

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => res.send("Hello, world!"));
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
+const commentRoute = require("./routes/commentRoute");
+
+//render home page
+
+app.use("/posts/:postId/comments", commentRoute);
+app.use("/posts", postRoute);
+
+app.get("/", (req, res) => {
+  res.redirect("/posts");
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
---
+++ backend/routes/postRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const postRouter = Router();
+const postController = require("../controllers/postController");
+
+postRouter.get("/", postController.getPostsByUser);
+postRouter.post("/", postController.createPost);
+
+postRouter.get("/:postId", postController.getPostById);
+postRouter.put("/:postId", postController.updatePost);
+postRouter.delete("/:postId", postController.deletePost);
+
+
+
